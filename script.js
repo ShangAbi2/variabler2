4bba6b97e7d420afc5768c637e9013c2153f060a
Big Poo <156303066+ShangAbi2@users.noreply.github.com>
2025-08-15 09:57:34 +0200

    init
    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var selectblå = document.getElementById("blå");
+var selectrød = document.getElementById("rød");   
+var selectgrønn = document.getElementById("grønn");
+var selectedColor = "";
+
+/*
+function selectColor(farge) {
+    selectedColor = farge;
+    if (farge.id === "blå") {
+        farge.textContent = "du valgte blå";
+        farge.style.color = "blue";
+         selectedColor = "blå";
+    } else if (farge.id === "rød") {
+        farge.textContent = "du valgte rød";
+        farge.style.color = "red";
+         selectedColor = "rød";
+    } else if (farge.id === "grønn") {
+        farge.textContent = "du valgte grønn";
+        farge.style.color = "green";
+         selectedColor = "grønn";
+    }
+}
+*/
+// best practice:
+const cssColors = {
+    blå: "blue",
+    rød: "red",
+    grønn: "green"
+};
+
+
+function selectColor(farge) {
+
+if (selectedColor) {
+        var prevEl = document.getElementById(selectedColor);
+        if (prevEl && prevEl !== farge) {
+            prevEl.textContent = "klikk her for å velge " + prevEl.id;
+            prevEl.style.color = "";
+        }
+    }
+
+    selectedColor = farge.id;
+    farge.textContent = "du aktiverte fargen:" + selectedColor;
+    farge.style.color = cssColors[farge.id];
+}
+
+function changeWholeBackground(){
+    if (selectedColor) {
+        document.body.style.backgroundColor = cssColors[selectedColor];
+    }
+}
